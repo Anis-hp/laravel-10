16603fd880f2e52a2c88a52162edb197d0b344cf
Anis-hp <webhp.co@gmail.com>
2024-01-31 18:56:19 +0330

    database queries

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,31 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('/' , function(){
+    // fetch all users
+    $users = DB::select('select * from users');
+
+    // create new user
+    // $user = DB::insert('insert into users (name, email, password) values (?,?,?)',[
+    //     'anis',
+    //     'webhp.co@gmail.com',
+    //     '@1234567890@'
+    // ]);
+
+    // user update
+    // $userupdate = DB::update("update users set email=? where id=?",[
+    //     'anishp13851@gmail.com' , 
+    //     '0'
+    // ]);
+
+    // delete user
+    // $userdelete = DB::delete('delete from user where id=?',[
+    //     '5'
+    // ]);
+    dd($users);
 });
 
 Route::get('/dashboard', function () {
